f94d76aac2d08d305b47bd4058458853bccfe6c1
paratechnology <parabolictechspace@gmail.com>
2026-03-04 10:36:01 +0100

    deploy

--- libs/shared-interfaces/src/baseurl.ts
+++ libs/shared-interfaces/src/baseurl.ts
@@ -1,1 +1,2 @@
-export const baseURL = 'http://localhost:8080/api/';
+// export const baseURL = 'http://localhost:8080/api/';
+export const baseURL = 'https://teddycityhotels-api-207712314730.africa-south1.run.app/api/';
